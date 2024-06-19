4cb3b326656ef558a6f06d09a478a51185e6a69a
HUS NAIN <husnain@ss-japan.com>
2024-06-19 10:42:44 +0900

    Complete Porfolio Web

---
+++ components/Grid.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { BsEmojiAngry } from 'react-icons/bs'
+import { gridItems } from '../data'
+
+const Grid = () => {
+  return (
+    <div>
+      <section id='about'>
+        <BentoGrid>
+            {gridItems.map(({id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
+                <BentoGridItem 
+                id={id}
+                key={id}
+                title={title}
+                description={description}
+                className={className}
+                img={img}
+                imgClassName={imgClassName}
+                titleClassName={titleClassName}
+                spareImg={spareImg}
+                />
+            ))}
+        </BentoGrid>
+      </section>
+    </div>
+  )
+}
+
+export default Grid;
